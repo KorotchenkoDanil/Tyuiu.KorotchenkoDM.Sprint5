84028a925c73bc5e9b6cad80cd8d2c2bad2948b5
Tyuiu.KorotchenkoDM@outlook.com <danil.korotchenko.2005@gmail.com>
2023-11-19 13:02:35 +0500

    Task2

---
+++ Tyuiu.KorotchenkoDM.Sprint5.Task2.V7.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.KorotchenkoDM.Sprint5.Task2.V7.Lib;
+
+namespace Tyuiu.KorotchenkoDM.Sprint5.Task2.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedSaveToFileTextData()
+        {
+            string path = @"C:\Users\ДАНИЛ\source\repos\Tyuiu.KorotchenkoDM.Sprint5\Tyuiu.KorotchenkoDM.Sprint5.Task2.V7\bin\Debug\OutPutFileTask2.scv";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
